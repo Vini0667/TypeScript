9b1f58f2d217c254840ade7afc29ed2108a9db66
Vini0667 <vini04.silva09+github@gmail.com>
2022-11-19 02:51:04 -0400

    Terminada a parte de tipos

--- TypeScript/TiposPrimitivos/AprendendoTipos.js
+++ TypeScript/TiposPrimitivos/AprendendoTipos.js
@@ -20,7 +20,7 @@ let num2 = 20;
 console.log(str);
 function func() { } // Cria funções -> function nomeDaFuncao (parametro1 : tipo, parametro2 : tipo...) : tipoRetorno {}
 if (bool != null) { } // Cria if -> if (condição) { bloco de comandos } 
-// Operadores Comparação -> '!' -> negação (Operador não) | "!=" -> diferente | "==" -> igual | "===" -> exatamente igual | "&&" operador e; "||" operador ou.
+// Operadores Comparação -> '!' -> Negação (Operador não) | "!=" -> diferente | "!==" -> Exatamente diferente | "==" -> Igual | "===" -> Exatamente igual | "&&" Operador e; "||" Operador ou.
 if (bool != undefined) { } // Tipo undefined
 if (bool) {
     let i;
@@ -35,8 +35,39 @@ tuple = ["Vinicius", 16]; // Só aceita os valores definidos acima
 console.log(tuple[0].length);
 var Day;
 (function (Day) {
-    Day[Day["MONDAY"] = 0] = "MONDAY";
-    Day[Day["TUESDAY"] = 1] = "TUESDAY";
-})(Day || (Day = {}));
-let day = Day.MONDAY;
-console.log(`Dia: ${day}`);
+    Day[Day["DOMINGO"] = 0] = "DOMINGO";
+    Day[Day["SEGUNDA"] = 1] = "SEGUNDA";
+    Day[Day["TER\u00C7A"] = 2] = "TER\u00C7A";
+})(Day || (Day = {})); // A enumeração são valores numericos, começando por 0 e vai até o número de valores.
+/*
+* Pode-se alterar o valor do elemento enumerado colocando o valor desejado desta forma
+** enum Day {DOMINGO = 4, SEGUNDA, TERÇA}
+* Assim domingo fica com valor 4, segunda vale 5 e terça vale 6.
+*/
+let day = Day.DOMINGO;
+console.log(`Dia: ${day} ${Day[0]} ${Day[1]} ${Day[2]}`); // Também é possivel mostrar o que está escrito na enumeração passando a posição desejada como no exemplo:
+/*
+*
+** Day[0] == DOMINGO, Day[1] == SEGUNDA e Day[2] == TERÇA.
+*
+*/
+function add(v1, v2, v3) {
+    if (v3 !== undefined) {
+        return v1 + v2 + v3;
+    }
+    return v1 + v2;
+} // O '?' informa que este valor não é obrigatório
+function add2(v1, v2, v3) {
+    let var1 = 10;
+} // O tipo any é um tipo indefinido e pode aceitar qualquer tipo
+let varVoid = undefined; // O void também é um tipo primitivo
+let var1 = `Olá`;
+let stringLength = var1.length; //(<tipo> variavel) -> É um cast que comprova que a variavel é daquele tipo primitivo 
+// Length é para medir o tamanho da string
+let stringUpperCase = var1.toLocaleUpperCase();
+function add3(v1, ...v2) {
+    let sum = 0;
+    for (let v3 = 0; v3 < v2.length; v3++) {
+        sum += v2[v3];
+    }
+} // Os 3 pontos são um "método varArgs" que é passado um Array | Operadores ++ -> Incrementa +1 | -- Decrementa -1
--- TypeScript/TiposPrimitivos/AprendendoTipos.ts
+++ TypeScript/TiposPrimitivos/AprendendoTipos.ts
@@ -31,7 +31,8 @@ console.log (str);
 function func () : void { } // Cria funções -> function nomeDaFuncao (parametro1 : tipo, parametro2 : tipo...) : tipoRetorno {}
 
 if (bool != null) {} // Cria if -> if (condição) { bloco de comandos } 
-// Operadores Comparação -> '!' -> negação (Operador não) | "!=" -> diferente | "==" -> igual | "===" -> exatamente igual | "&&" operador e; "||" operador ou.
+// Operadores Comparação -> '!' -> Negação (Operador não) | "!=" -> diferente | "!==" -> Exatamente diferente | "==" -> Igual | "===" -> Exatamente igual | "&&" Operador e; "||" Operador ou.
+
 if (bool != undefined) {} // Tipo undefined
 
 if (bool) {
@@ -50,9 +51,45 @@ tuple = ["Vinicius", 16]; // Só aceita os valores definidos acima
 
 console.log (tuple[0].length);
 
-enum Day {MONDAY, TUESDAY}
+enum Day {DOMINGO, SEGUNDA, TERÇA} // A enumeração são valores numericos, começando por 0 e vai até o número de valores.
+/* 
+* Pode-se alterar o valor do elemento enumerado colocando o valor desejado desta forma
+** enum Day {DOMINGO = 4, SEGUNDA, TERÇA}
+* Assim domingo fica com valor 4, segunda vale 5 e terça vale 6. 
+*/
 
-let day : Day = Day.MONDAY;
+let day : Day = Day.DOMINGO;
 
-console.log (`Dia: ${day}`);
+console.log (`Dia: ${day} ${Day[0]} ${Day[1]} ${Day[2]}`); // Também é possivel mostrar o que está escrito na enumeração passando a posição desejada como no exemplo:
+/* 
+*
+** Day[0] == DOMINGO, Day[1] == SEGUNDA e Day[2] == TERÇA.
+*
+*/
 
+function add (v1 : number, v2 : number, v3 ? : number) : number {
+    if (v3 !== undefined) {
+       return v1 + v2 + v3;
+    }
+    return v1 + v2;
+} // O '?' informa que este valor não é obrigatório
+
+function add2 (v1 : any, v2 : any, v3 ? : any) : void {
+    let var1 : any = 10;
+} // O tipo any é um tipo indefinido e pode aceitar qualquer tipo
+
+let varVoid : void = undefined; // O void também é um tipo primitivo
+
+let var1 : any = `Olá`;
+
+let stringLength : number = (<string> var1).length; //(<tipo> variavel) -> É um cast que comprova que a variavel é daquele tipo primitivo 
+// Length é para medir o tamanho da string
+let stringUpperCase : string = (<string> var1).toLocaleUpperCase ();
+
+
+function add3 (v1 : string, ...v2 : number[]) : void {
+    let sum : number = 0;
+    for (let v3 = 0; v3 < v2.length; v3++) {
+        sum += v2[v3];
+    }
+} // Os 3 pontos são um "método varArgs" que é passado um Array | Operadores ++ -> Incrementa +1 | -- Decrementa -1
